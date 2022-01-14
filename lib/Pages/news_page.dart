7bb7fbe5e5b30355d20504738597cbe8ad0d31c2
muathye <muath.ye@gmail.com>
2022-01-14 14:45:31 +0300

    Add NewsPage

---
+++ lib/Pages/news_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '/BLoCs/news_bloc.dart';
+import '/Models/news_model.dart';
+
+class NewsPage extends StatefulWidget {
+  const NewsPage({Key? key}) : super(key: key);
+
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  final newsBloc = NewsBloc();
+
+  @override
+  void initState() {
+    newsBloc.eventSink.add(NewsAction.Fetch);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+      body: Container(
+        child: StreamBuilder<List<Article>>(
+          stream: newsBloc.stateStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    var article = snapshot.data![index];
+                    var formattedTime = DateFormat('dd MMM - HH:mm')
+                        .format(article.publishedAt ?? DateTime.now());
+                    return Container(
+                      height: 100,
+                      margin: const EdgeInsets.all(8),
+                      child: Row(
+                        children: <Widget>[
+                          Card(
+                            clipBehavior: Clip.antiAlias,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: AspectRatio(
+                                aspectRatio: 1,
+                                child: Image.network(
+                                  article.urlToImage ?? '',
+                                  fit: BoxFit.cover,
+                                )),
+                          ),
+                          SizedBox(width: 16),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(formattedTime),
+                                Text(
+                                  article.title ?? '',
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  article.description ?? '',
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            } else
+              return Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
+    );
+  }
+}
