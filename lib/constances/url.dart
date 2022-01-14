dc9438e8a5011899573fafc1cbd933f98d2dcc13
muathye <muath.ye@gmail.com>
2022-01-14 14:44:57 +0300

    Add url cons

---
+++ lib/constances/url.dart
@@ -0,0 +1,5 @@
+class URL {
+  static String _domainUrl = 'http://newsapi.org/v2/';
+  static String _apiKey = '09f8c74c4ec44c78bb006b068071c5c7';
+  static String newsUrl = _domainUrl + "everything?domains=wsj.com&apiKey=$_apiKey";
+}
